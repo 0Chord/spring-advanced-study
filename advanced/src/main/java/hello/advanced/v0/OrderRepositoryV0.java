3f04d46be56a7d0f0283e81a8194023571a9323c
0Chord <seaweed.0chord@gmail.com>
2023-08-07 19:55:58 +0900

    feat : 추가 로그추적기V0

--- advanced/src/main/java/hello/advanced/v0/OrderRepositoryV0.java
+++ advanced/src/main/java/hello/advanced/v0/OrderRepositoryV0.java
@@ -1,2 +1,27 @@
-package hello.advanced.v0;public class OrderRepositoryV0 {
+package hello.advanced.v0;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+	public void save(String itemId) {
+		if (itemId.equals("ex")) {
+			throw new IllegalStateException("예외 발생!");
+		}
+
+		sleep(1000);
+	}
+
+	private static void sleep(int millis) {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
 }
