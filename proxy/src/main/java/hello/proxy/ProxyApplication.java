40605ee4962dde03319884ed7ce49be4ee1c5ca3
0Chord <seaweed.0chord@gmail.com>
2023-08-29 13:16:18 +0900

    feat : 추가 스프링 자동 빈 후처리기

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Import;
 
 import hello.proxy.config.ProxyFactoryConfigV2;
 import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 
@@ -18,7 +19,8 @@ import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 // @Import(DynamicProxyFilterConfig.class)
 // @Import(ProxyFactoryConfigV1.class)
 // @Import(ProxyFactoryConfigV2.class)
-@Import(BeanPostProcessorConfig.class)
+// @Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
